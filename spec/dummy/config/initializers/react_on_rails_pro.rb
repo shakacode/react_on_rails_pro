916196e3ed6dc3e7caa5338eb987dee061c78a91
Justin Gordon <justin.gordon@gmail.com>
2018-05-05 22:36:31 -1000

    WIP

--- spec/dummy/config/initializers/react_on_rails_pro.rb
+++ spec/dummy/config/initializers/react_on_rails_pro.rb
@@ -2,5 +2,4 @@ ReactOnRailsPro.configure do |config|
   config.renderer_port = 3800
   config.server_render_method = "VmRenderer"
   config.use_fallback_renderer_exec_js = false
-  # config.renderer_port = 6081
 end
