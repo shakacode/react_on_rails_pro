1372a34815fafcff5903f4506ff5db836caa85e1
Andrey Ashgaliyev <a.ashgaliev@gmail.com>
2020-02-12 12:52:16 +0500

    Change network error test to use webmock

--- spec/dummy/spec/requests/upload_asset_spec.rb
+++ spec/dummy/spec/requests/upload_asset_spec.rb
@@ -3,19 +3,14 @@
 require "rails_helper"
 
 describe "Upload asset" do
+
   let(:fixture_path) { File.expand_path('./spec/fixtures/sample.json') }
   let(:non_exist_fixture_path) { File.expand_path('./spec/fixtures/sample99.json') }
   let(:asset_path) { File.expand_path('./public/webpack/production/sample.json') }
+  let!(:orig_renderer_url) { ReactOnRailsPro.configuration.renderer_url }
 
   before(:each) do
     File.delete(asset_path) if File.exist?(asset_path)
-  end
-
-  after(:all) do
-    ReactOnRailsPro.configure do |config|
-      config.renderer_url = "https://localhost:3800"
-    end
-    ReactOnRailsPro::RequestHelper.reset_connection
   end
 
   it "copying asset to public folder" do
@@ -31,13 +26,12 @@ describe "Upload asset" do
   end
 
   it "throws error if can't connect to vm-renderer" do
-    invalid_url = "http://localhost:99999"
+    WebMock.disable_net_connect!(allow_localhost: false)
+    stubbed_request = stub_request(:any, /upload-asset/).
+        to_timeout
     expect do
-      ReactOnRailsPro.configure do |config|
-        config.renderer_url = invalid_url
-      end
-      ReactOnRailsPro::RequestHelper.reset_connection
       ReactOnRailsPro::RequestHelper.upload_asset(fixture_path, "application/json")
     end.to raise_exception(ReactOnRailsPro::Error)
+    WebMock.allow_net_connect!
   end
 end
