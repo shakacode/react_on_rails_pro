bd7c98817d9b0ff6c9a6919bb8f10232fed23271
Andrey Ashgaliyev <a.ashgaliev@gmail.com>
2020-05-22 18:02:29 +0500

    Add Buffer, process to context

--- packages/vm-renderer/src/worker/vm.js
+++ packages/vm-renderer/src/worker/vm.js
@@ -73,7 +73,7 @@ exports.buildVM = async function buildVM(filePath) {
   try {
     const { supportModules } = getConfig();
     vmBundleFilePath = undefined;
-    context = vm.createContext();
+    context = vm.createContext({ Buffer, process });
     // Create explicit reference to global context, just in case (some libs can use it):
     vm.runInContext('global = this', context);
 
