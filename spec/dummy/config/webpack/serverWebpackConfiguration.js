8a44c31d0f2f402a65e99d640bad93f343d7a263
Justin Gordon <justin.gordon@gmail.com>
2021-01-23 14:14:35 -1000

    Change the serverWebpackConfig target to node
    For spec/dummy

--- spec/dummy/config/webpack/serverWebpackConfiguration.js
+++ spec/dummy/config/webpack/serverWebpackConfiguration.js
@@ -95,7 +95,7 @@ const configureServer = () => {
   // If using the default 'web', then libraries like Emotion and loadable-components
   // break with SSR. The fix is to use a node renderer and change the target.
   // If using the React on Rails Pro node server renderer, uncomment the next line
-  // serverWebpackConfig.target = 'node'
+  serverWebpackConfig.target = 'node';
 
   return serverWebpackConfig;
 };
