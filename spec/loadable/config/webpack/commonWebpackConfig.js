59c35f4b0189f2528f5104d10fb40cf8b414707f
Gus <gsc13@tuta.io>
2021-12-13 12:13:24 -0300

    Update webpack config files

---
+++ spec/loadable/config/webpack/commonWebpackConfig.js
@@ -0,0 +1,19 @@
+const { webpackConfig: baseClientWebpackConfig, merge } = require('@rails/webpacker');
+const webpack = require('webpack');
+
+const commonOptions = {
+  resolve: {
+    extensions: ['.css', '.ts', '.tsx'],
+    fallback: { "path": false, "fs": false }
+  },
+};
+
+baseClientWebpackConfig.plugins.push(
+  new webpack.ProvidePlugin({
+    path: 'path-browserify',
+  })
+)
+
+const commonWebpackConfig = () => merge({}, baseClientWebpackConfig, commonOptions);
+
+module.exports = commonWebpackConfig;
