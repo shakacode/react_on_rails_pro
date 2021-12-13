f4c608dc58bdfcd9cb3a895d1bcae0feb9cf3754
Gus <gsc13@tuta.io>
2021-12-13 11:31:45 -0300

    Add Webpack configuration and Install dependencies

---
+++ spec/dummy/config/webpack/commonWebpackConfig.js
@@ -0,0 +1,69 @@
+const { webpackConfig: baseClientWebpackConfig, merge } = require('@rails/webpacker');
+const webpack = require('webpack');
+
+const sassResources = ['./client/app/assets/styles/app-variables.scss'];
+const aliasConfig = require('./alias.js');
+
+const commonOptions = {
+  resolve: {
+    extensions: ['.css', '.ts', '.tsx'],
+    fallback: { "path": false, "fs": false }
+  },
+};
+
+const isWebpackDevServer = process.env.WEBPACK_DEV_SERVER;
+
+// For details on the pros and cons of inlining images:
+// https://developers.google.com/web/fundamentals/design-and-ux/responsive/images
+// https://survivejs.com/webpack/loading/images/
+// Normally below 1k, inline. We're making the example bigger to show a both inlined and non-inlined images
+
+// rules
+const sassLoaderConfig = {
+  loader: 'sass-resources-loader',
+  options: {
+    resources: sassResources,
+  },
+};
+
+const scssConfigIndex = baseClientWebpackConfig.module.rules.findIndex((config) =>
+  '.scss'.match(config.test),
+);
+baseClientWebpackConfig.module.rules[scssConfigIndex].use.push(sassLoaderConfig);
+
+//baseClientWebpackConfig.splitChunks();
+if (isWebpackDevServer) {
+  baseClientWebpackConfig.plugins.push(
+    'NormalModuleReplacement',
+    new webpack.NormalModuleReplacementPlugin(/(.*)\.imports-loadable(\.jsx)?/, (resource) => {
+      /* eslint-disable no-param-reassign */
+      resource.request = resource.request.replace(/imports-loadable/, 'imports-hmr');
+      /* eslint-enable no-param-reassign */
+      return resource.request;
+    }),
+  );
+}
+
+baseClientWebpackConfig.plugins.push(
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+  }),
+);
+
+// add jquery
+const exposeJQuery = {
+  test: require.resolve('jquery'),
+  use: [{ loader: 'expose-loader', options: { exposes: ['$', 'jQuery'] } }],
+};
+
+const jqueryUjsLoader = {
+  test: require.resolve('jquery-ujs'),
+  use: [{ loader: 'imports-loader', options: { type: 'commonjs', imports: 'single jquery jQuery' } }],
+};
+
+baseClientWebpackConfig.module.rules.push(exposeJQuery, jqueryUjsLoader);
+
+const commonWebpackConfig = () => merge({}, baseClientWebpackConfig, commonOptions, aliasConfig);
+
+module.exports = commonWebpackConfig;
