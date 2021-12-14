0cec21063da306105ce090663b5d32f317d14692
Judah Meek <judah.meek@gmail.com>
2021-12-14 05:20:43 -0600

    fix HMR

--- spec/loadable/config/webpack/clientWebpackConfig.js
+++ spec/loadable/config/webpack/clientWebpackConfig.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack')
 const commonWebpackConfig = require('./commonWebpackConfig');
 const LoadablePlugin = require('@loadable/webpack-plugin');
 
@@ -14,6 +15,13 @@ const configureClient = () => {
 
   if (!isHMR) {
     clientConfig.plugins.unshift(new LoadablePlugin({ filename: 'loadable-stats.json', writeToDisk: true }));
+  } else {
+    clientConfig.plugins.unshift(new webpack.NormalModuleReplacementPlugin(/(.*)\.imports-loadable(\.jsx)?/, (resource) => {
+      /* eslint-disable no-param-reassign */
+      resource.request = resource.request.replace(/imports-loadable/, 'imports-hmr');
+      /* eslint-enable no-param-reassign */
+      return resource.request;
+    }))
   }
 
   return clientConfig;
