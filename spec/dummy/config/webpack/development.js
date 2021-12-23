a65a4dfbf06ac4f6d38d770e66826622a94fc268
Judah Meek <judah.meek@gmail.com>
2021-12-23 14:38:26 -0600

    remove redundant config

--- spec/dummy/config/webpack/development.js
+++ spec/dummy/config/webpack/development.js
@@ -5,24 +5,7 @@ const { devServer, inliningCss } = require('@rails/webpacker');
 const webpackConfig = require('./webpackConfig');
 
 const developmentEnvOnly = (clientWebpackConfig, _serverWebpackConfig) => {
-  const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
-  const isHMR = process.env.HMR;
-
-  //plugins
-  if (isHMR) {
-    clientWebpackConfig.plugins.push(
-      new ReactRefreshWebpackPlugin({
-        overlay: {
-          sockPort: devServer.port,
-        },
-      }),
-    );
-  }
-
   if (inliningCss) {
-    // Note, when this is run, we're building the server and client bundles in separate processes.
-    // Thus, this plugin is not applied.
-
     // eslint-disable-next-line global-require
     const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
     clientWebpackConfig.plugins.push(
