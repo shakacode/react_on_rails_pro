8b5e0492cfb5b9791cb0e4255e6956cb05d2ecc3
Judah Meek <judah.meek@gmail.com>
2022-08-02 15:31:35 -0500

    this doesnt work either
    took logic from 
    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function

--- spec/dummy/client/app/startup/SetTimeoutLoggingApp.jsx
+++ spec/dummy/client/app/startup/SetTimeoutLoggingApp.jsx
@@ -7,7 +7,11 @@ import { renderToString } from 'react-dom/server';
  export default async (_props, _railsContext) => {
    // eslint-disable-next-line no-console
    let x = 'initial value';
-   setTimeout(x = 'value set by setTimeout', 1);
+   x = await new Promise(resolve => {
+    setTimeout(() => {
+      resolve('value set by setTimeout');
+    }, 1);
+  });
    const Komponent = <div>Called setTimeout and returned this {x}.</div>
    return {
      componentHtml: renderToString(<Komponent />)
