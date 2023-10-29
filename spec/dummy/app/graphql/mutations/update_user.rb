932b792393418196c197ef22d3ea58eeeae07138
Abanoub Ghadban <abanoub@shakacode.com>
2023-10-29 10:06:09 +0200

    fix linting problems

--- spec/dummy/app/graphql/mutations/update_user.rb
+++ spec/dummy/app/graphql/mutations/update_user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Mutations
   class UpdateUser < BaseMutation
     # Return Type
--- spec/dummy/app/graphql/types/mutation_type.rb
+++ spec/dummy/app/graphql/types/mutation_type.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Types
   class MutationType < Types::BaseObject
     field :update_user, mutation: Mutations::UpdateUser
