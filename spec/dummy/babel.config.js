305d639eaea237ccc27baf13ec9adf0851ae8b5a
Abanoub Ghadban <abanoub@shakacode.com>
2023-10-29 10:13:31 +0200

    fix linting problems

--- spec/dummy/babel.config.js
+++ spec/dummy/babel.config.js
@@ -14,8 +14,9 @@ module.exports = function (api) {
         },
       ],
     ].filter(Boolean),
-    plugins: [[
-      'macros',
+    plugins: [
+      [
+        'macros',
         {
           useSSRComputation: {
             side,
