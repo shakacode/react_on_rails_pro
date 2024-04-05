c815ae1bcc34db067f02130e5fe7aa32a4666755
Alexey Romanov <alexey.v.romanov@gmail.com>
2024-04-05 14:19:33 +0000

    Finish converting test helper

--- packages/node-renderer/tests/helper.ts
+++ packages/node-renderer/tests/helper.ts
@@ -1,5 +1,3 @@
-/* eslint-disable */
-// @ts-nocheck
 // NOTE: The tmp bundle directory for each test file must be different due to the fact that
 // jest will run multiple test files synchronously.
 import path from 'path';
@@ -28,66 +26,62 @@ export function getOtherFixtureAsset() {
   return path.resolve(__dirname, `./fixtures/${ASSET_UPLOAD_OTHER_FILE}`);
 }
 
-export function bundlePath(testName) {
+export function bundlePath(testName: string) {
   return path.resolve(__dirname, 'tmp', testName);
 }
 
-export function setConfig(testName) {
+export function setConfig(testName: string) {
   buildConfig({
-    bundlePath: exports.bundlePath(testName),
+    bundlePath: bundlePath(testName),
   });
 }
 
-/**
- *
- * @returns {Promise<void>}
- */
-export async function createVmBundle(testName) {
-  await fsCopyFileAsync(exports.getFixtureBundle(), exports.vmBundlePath(testName));
-  return buildVM(exports.vmBundlePath(testName));
+export function vmBundlePath(testName: string) {
+  return path.resolve(bundlePath(testName), `${BUNDLE_TIMESTAMP}.js`);
 }
 
-export function lockfilePath(testName) {
-  return `${exports.vmBundlePath(testName)}.lock`;
+export async function createVmBundle(testName: string) {
+  await fsCopyFileAsync(getFixtureBundle(), vmBundlePath(testName));
+  return buildVM(vmBundlePath(testName));
 }
 
-export function uploadedBundleDir(testName) {
-  return path.resolve(exports.bundlePath(testName), 'uploads');
+export function lockfilePath(testName: string) {
+  return `${vmBundlePath(testName)}.lock`;
 }
 
-export function uploadedBundlePath(testName) {
-  return path.resolve(exports.uploadedBundleDir(testName), `${exports.BUNDLE_TIMESTAMP}.js`);
+export function uploadedBundleDir(testName: string) {
+  return path.resolve(bundlePath(testName), 'uploads');
 }
 
-export function vmBundlePath(testName) {
-  return path.resolve(exports.bundlePath(testName), `${exports.BUNDLE_TIMESTAMP}.js`);
+export function uploadedBundlePath(testName: string) {
+  return path.resolve(uploadedBundleDir(testName), `${BUNDLE_TIMESTAMP}.js`);
 }
 
-export function assetPath(testName) {
-  return path.resolve(exports.bundlePath(testName), exports.ASSET_UPLOAD_FILE);
+export function assetPath(testName: string) {
+  return path.resolve(bundlePath(testName), ASSET_UPLOAD_FILE);
 }
 
-export function assetPathOther(testName) {
-  return path.resolve(exports.bundlePath(testName), exports.ASSET_UPLOAD_OTHER_FILE);
+export function assetPathOther(testName: string) {
+  return path.resolve(bundlePath(testName), ASSET_UPLOAD_OTHER_FILE);
 }
 
-export async function createUploadedBundle(testName) {
+export async function createUploadedBundle(testName: string) {
   const mkdirAsync = promisify(fs.mkdir);
-  await mkdirAsync(exports.uploadedBundleDir(testName), { recursive: true });
-  return fsCopyFileAsync(exports.getFixtureBundle(), exports.uploadedBundlePath(testName));
+  await mkdirAsync(uploadedBundleDir(testName), { recursive: true });
+  return fsCopyFileAsync(getFixtureBundle(), uploadedBundlePath(testName));
 }
 
-export async function createAsset(testName) {
-  return fsCopyFileAsync(exports.getFixtureAsset(), exports.assetPath(testName));
+export async function createAsset(testName: string) {
+  return fsCopyFileAsync(getFixtureAsset(), assetPath(testName));
 }
 
-export async function resetForTest(testName) {
-  await fsExtra.emptyDir(exports.bundlePath(testName));
+export async function resetForTest(testName: string) {
+  await fsExtra.emptyDir(bundlePath(testName));
   resetVM();
-  exports.setConfig(testName);
+  setConfig(testName);
 }
 
-export function readRenderingRequest(projectName, commit, requestDumpFileName) {
+export function readRenderingRequest(projectName: string, commit: string, requestDumpFileName: string) {
   const renderingRequestRelativePath = path.join(
     './fixtures/projects/',
     projectName,
@@ -95,27 +89,6 @@ export function readRenderingRequest(projectName, commit, requestDumpFileName) {
     requestDumpFileName,
   );
   return fs.readFileSync(path.resolve(__dirname, renderingRequestRelativePath), 'utf8');
-}
-
-export function createResponse(validate) {
-  const result = {
-    headers: {},
-    data: '',
-    status: null,
-  };
-
-  return {
-    set: (key, value) => {
-      result.headers[key] = value;
-    },
-    status: (value) => {
-      result.status = value;
-    },
-    send: (data) => {
-      result.data = data;
-      validate(result);
-    },
-  };
 }
 
 setConfig('helper');
