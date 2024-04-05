83ca4c0d850758b81d747f7393ec57127933d865
Alexey Romanov <alexey.v.romanov@gmail.com>
2024-04-05 14:16:03 +0000

    Begin to convert test helper

--- packages/node-renderer/tests/helper.ts
+++ packages/node-renderer/tests/helper.ts
@@ -1,93 +1,93 @@
+/* eslint-disable */
+// @ts-nocheck
 // NOTE: The tmp bundle directory for each test file must be different due to the fact that
 // jest will run multiple test files synchronously.
-const path = require('path');
-const fs = require('fs');
-const { promisify } = require('util');
-const fsExtra = require('fs-extra');
+import path from 'path';
+import fs from 'fs';
+import { promisify } from 'util';
+import fsExtra from 'fs-extra';
+import { buildVM, resetVM } from '../src/worker/vm';
+import { buildConfig } from '../src/shared/configBuilder';
 
 const fsCopyFileAsync = promisify(fs.copyFile);
 
-const { buildVM, resetVM } = require('../src/worker/vm');
+export const BUNDLE_TIMESTAMP = 1495063024898;
 
-const { buildConfig } = require('../src/shared/configBuilder');
+export const ASSET_UPLOAD_FILE = 'loadable-stats.json';
+export const ASSET_UPLOAD_OTHER_FILE = 'loadable-stats-other.json';
 
-exports.BUNDLE_TIMESTAMP = 1495063024898;
-
-exports.ASSET_UPLOAD_FILE = 'loadable-stats.json';
-exports.ASSET_UPLOAD_OTHER_FILE = 'loadable-stats-other.json';
-
-exports.getFixtureBundle = function getFixtureBundle() {
+export function getFixtureBundle() {
   return path.resolve(__dirname, './fixtures/bundle.js');
-};
+}
 
-exports.getFixtureAsset = function getFixtureAsset() {
-  return path.resolve(__dirname, `./fixtures/${exports.ASSET_UPLOAD_FILE}`);
-};
+export function getFixtureAsset() {
+  return path.resolve(__dirname, `./fixtures/${ASSET_UPLOAD_FILE}`);
+}
 
-exports.getOtherFixtureAsset = function getOtherFixtureAsset() {
-  return path.resolve(__dirname, `./fixtures/${exports.ASSET_UPLOAD_OTHER_FILE}`);
-};
+export function getOtherFixtureAsset() {
+  return path.resolve(__dirname, `./fixtures/${ASSET_UPLOAD_OTHER_FILE}`);
+}
 
-exports.bundlePath = function bundlePath(testName) {
+export function bundlePath(testName) {
   return path.resolve(__dirname, 'tmp', testName);
-};
+}
 
-exports.setConfig = function setConfig(testName) {
+export function setConfig(testName) {
   buildConfig({
     bundlePath: exports.bundlePath(testName),
   });
-};
+}
 
 /**
  *
  * @returns {Promise<void>}
  */
-exports.createVmBundle = async function createVmBundle(testName) {
+export async function createVmBundle(testName) {
   await fsCopyFileAsync(exports.getFixtureBundle(), exports.vmBundlePath(testName));
   return buildVM(exports.vmBundlePath(testName));
-};
+}
 
-exports.lockfilePath = function lockfilePath(testName) {
+export function lockfilePath(testName) {
   return `${exports.vmBundlePath(testName)}.lock`;
-};
+}
 
-exports.uploadedBundleDir = function uploadedBundleDir(testName) {
+export function uploadedBundleDir(testName) {
   return path.resolve(exports.bundlePath(testName), 'uploads');
-};
+}
 
-exports.uploadedBundlePath = function uploadedBundlePath(testName) {
+export function uploadedBundlePath(testName) {
   return path.resolve(exports.uploadedBundleDir(testName), `${exports.BUNDLE_TIMESTAMP}.js`);
-};
+}
 
-exports.vmBundlePath = function vmBundlePath(testName) {
+export function vmBundlePath(testName) {
   return path.resolve(exports.bundlePath(testName), `${exports.BUNDLE_TIMESTAMP}.js`);
-};
+}
 
-exports.assetPath = function assetPath(testName) {
+export function assetPath(testName) {
   return path.resolve(exports.bundlePath(testName), exports.ASSET_UPLOAD_FILE);
-};
+}
 
-exports.assetPathOther = function assetPathOther(testName) {
+export function assetPathOther(testName) {
   return path.resolve(exports.bundlePath(testName), exports.ASSET_UPLOAD_OTHER_FILE);
-};
+}
 
-exports.createUploadedBundle = async function createUploadedBundle(testName) {
+export async function createUploadedBundle(testName) {
   const mkdirAsync = promisify(fs.mkdir);
   await mkdirAsync(exports.uploadedBundleDir(testName), { recursive: true });
   return fsCopyFileAsync(exports.getFixtureBundle(), exports.uploadedBundlePath(testName));
-};
+}
 
-exports.createAsset = async function createAsset(testName) {
+export async function createAsset(testName) {
   return fsCopyFileAsync(exports.getFixtureAsset(), exports.assetPath(testName));
-};
+}
 
-exports.resetForTest = async function resetForTest(testName) {
+export async function resetForTest(testName) {
   await fsExtra.emptyDir(exports.bundlePath(testName));
   resetVM();
   exports.setConfig(testName);
-};
+}
 
-exports.readRenderingRequest = function readRenderingRequest(projectName, commit, requestDumpFileName) {
+export function readRenderingRequest(projectName, commit, requestDumpFileName) {
   const renderingRequestRelativePath = path.join(
     './fixtures/projects/',
     projectName,
@@ -95,9 +95,9 @@ exports.readRenderingRequest = function readRenderingRequest(projectName, commit
     requestDumpFileName,
   );
   return fs.readFileSync(path.resolve(__dirname, renderingRequestRelativePath), 'utf8');
-};
+}
 
-exports.createResponse = function createResponse(validate) {
+export function createResponse(validate) {
   const result = {
     headers: {},
     data: '',
@@ -116,6 +116,6 @@ exports.createResponse = function createResponse(validate) {
       validate(result);
     },
   };
-};
+}
 
-exports.setConfig('helper');
+setConfig('helper');
