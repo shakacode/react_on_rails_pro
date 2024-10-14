cc50baf3e171bb12f081ff5c0295c474d3de0597
Abanoub Ghadban <abanoub@shakacode.com>
2024-10-14 16:12:45 +0300

    update setTimout example

--- spec/dummy/client/app/ror-auto-load-components/SetTimeoutLoggingApp.server.jsx
+++ spec/dummy/client/app/ror-auto-load-components/SetTimeoutLoggingApp.server.jsx
@@ -5,11 +5,14 @@ import { renderToString } from 'react-dom/server';
  * TODO: Node rendering server should handle a timeout.
  */
 export default async (_props, _railsContext) => {
-  const delayedValue = await new Promise((resolve) => {
-    setTimeout(() => {
-      resolve('this value is set by setTimeout during SSR');
-    }, 1);
-  });
-  const element = <div>Disable javascript in your browser options to confirm {delayedValue}.</div>;
+  console.log(`Console log from Sync Server`);
+
+  for (let i = 0; i < 20; i++) {
+    await new Promise((resolve) => setTimeout(resolve, 100));
+    console.log(`Console log from Simple Async Function at iteration ${i}`);
+  }
+
+  console.log(`Console log from Async Server after calling async functions`);
+  const element = <div>Disable javascript in your browser options to confirm this value is set by setTimeout during SSR.</div>;
   return renderToString(element);
 };
