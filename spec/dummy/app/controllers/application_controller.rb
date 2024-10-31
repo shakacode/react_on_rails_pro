38c14f4b85e93403ab450c0a95f4c503d1706dfc
Abanoub Ghadban <abanoub@shakacode.com>
2024-10-31 15:48:52 +0300

    make requested changes

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -11,27 +11,33 @@ class ApplicationController < ActionController::Base
     Rails.logger.error(err.backtrace.join("\n"))
 
     if response.committed?
-      Rails.logger.error("Error occurred after part of the response already sent, " \
-                         "the error should happen during streaming react component " \
-                         "and the error happen outside the shell")
+      Rails.logger.error(<<~ERROR)
+        Error occurred after part of the response already sent,
+        the error should happen during streaming react component
+        and the error happen outside the shell
+      ERROR
 
-      error_message = "<h2>Server-Side Rendering Error</h2>"
-      error_message += "<p>We apologize, but an error occurred while rendering " \
-                       "the page on the server.</p>"
-      error_message += "<p>If you are not redirected, please " \
-                       "<a href=\"#{server_side_log_throw_raise_invoker_path}\">" \
-                       "click here</a>.</p>"
+      error_message = <<~HTML
+        <h2>Server-Side Rendering Error</h2>
+        <p>We apologize, but an error occurred while rendering the page on the server.</p>
+        <p>If you are not redirected, please
+        <a href="#{server_side_log_throw_raise_invoker_path}">click here</a>.</p>
+      HTML
 
-      # Attempt to redirect using JavaScript
-      js_redirect = "<script>"
-      js_redirect += "document.getElementById('page-container').innerHTML = '#{error_message}';"
-      js_redirect += "setTimeout(function() { " \
-                     "window.location.href = '#{server_side_log_throw_raise_invoker_path}'; " \
-                     "}, 5000);"
-      js_redirect += "</script>"
+      js_redirect = <<~JAVASCRIPT
+        <script>
+          document.getElementById('page-container').innerHTML = #{ActiveSupport::JSON.encode(error_message)};
+          setTimeout(function() {
+            window.location.href = '#{server_side_log_throw_raise_invoker_path}';
+          }, 5000);
+        </script>
+      JAVASCRIPT
+      puts "js_redirect"
+      puts js_redirect
 
-      # Fallback to meta refresh if JavaScript is not supported
-      meta_refresh = "<meta http-equiv='refresh' content='5;url=#{server_side_log_throw_raise_invoker_path}'>"
+      meta_refresh = <<~HTML
+        <meta http-equiv='refresh' content='5;url=#{server_side_log_throw_raise_invoker_path}'>
+      HTML
 
       # Write error message, JavaScript redirect, and meta refresh
       response.stream.write(error_message + js_redirect + meta_refresh)
