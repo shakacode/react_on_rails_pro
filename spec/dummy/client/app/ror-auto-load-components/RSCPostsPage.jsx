eacfcf92ca3e6b80d3e656505f1a477da7bc2747
Abanoub Ghadban <abanoub@shakacode.com>
2024-11-19 13:27:36 +0200

    add stateful component to RSC page and add artificial delay

--- spec/dummy/client/app/ror-auto-load-components/RSCPostsPage.jsx
+++ spec/dummy/client/app/ror-auto-load-components/RSCPostsPage.jsx
@@ -1,9 +1,11 @@
 import React, { Suspense } from "react";
 import Posts from "../components/RSCPostsPage/Posts";
+import HelloWorld from "../components/HelloWorld.jsx";
 
-const RSCPostsPage = () => {
+const RSCPostsPage = (props) => {
   return (
     <div>
+      <HelloWorld {...props} />
       <h1 style={{ fontSize: "2rem", fontWeight: "bold" }}>RSC Posts Page</h1>
       <Suspense fallback={<div>Loading Posts...</div>}>
         <Posts />
