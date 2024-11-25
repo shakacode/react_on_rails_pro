c12a968a63bedc929e94e022a935d69907117943
Alexey Romanov <alexey.v.romanov@gmail.com>
2024-11-25 08:53:50 +0000

    Add public API for custom integrations

---
+++ packages/node-renderer/src/integrations/api.ts
@@ -0,0 +1,7 @@
+export { addErrorNotifier, addMessageNotifier, addNotifier } from '../shared/errorReporter';
+export {
+  setupTracing,
+  TracingContext,
+  TracingIntegrationOptions,
+  UnitOfWorkOptions,
+} from '../shared/tracing';
