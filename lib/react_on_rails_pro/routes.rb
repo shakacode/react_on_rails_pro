f96b16b0174ad93942f843dc067037799af39808
Abanoub Ghadban <abanoub@shakacode.com>
2024-12-05 17:16:31 +0200

    create a view and controller inside the gem that handles rsc rendering

---
+++ lib/react_on_rails_pro/routes.rb
@@ -0,0 +1,7 @@
+module ReactOnRailsPro
+  module Routes
+    def rsc_route(path: 'rsc', controller: 'react_on_rails_pro/rsc', **options)
+      get "#{path}/:component_name", to: "#{controller}#rsc", as: options[:as] || :rsc
+    end
+  end
+end
