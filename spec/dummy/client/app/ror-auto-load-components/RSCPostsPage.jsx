5a3306def96ead857fd6d91bd738b267b36eb291
Abanoub Ghadban <abanoub@shakacode.com>
2024-12-11 20:58:24 +0200

    add error boundary component

--- spec/dummy/client/app/ror-auto-load-components/RSCPostsPage.jsx
+++ spec/dummy/client/app/ror-auto-load-components/RSCPostsPage.jsx
@@ -1,16 +1,20 @@
 import React, { Suspense } from "react";
+import { ErrorBoundary } from "../components/ErrorBoundary";
 import Posts from "../components/RSCPostsPage/Posts";
 import HelloWorld from "../components/HelloWorld.jsx";
+import ErrorComponent from "../components/ErrorComponent.jsx";
 
 const RSCPostsPage = (props) => {
   return (
-    <div>
-      <HelloWorld {...props} />
+    <ErrorBoundary FallbackComponent={ErrorComponent}> 
+      <div>
+        <HelloWorld {...props} />
       <h1 style={{ fontSize: "2rem", fontWeight: "bold" }}>RSC Posts Page</h1>
       <Suspense fallback={<div>Loading Posts...</div>}>
         <Posts />
-      </Suspense>
-    </div>
+        </Suspense>
+      </div>
+    </ErrorBoundary>
   )
 }
 
