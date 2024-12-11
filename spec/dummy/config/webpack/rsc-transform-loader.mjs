14ed847a4e15e6f0d4d01647e76dd03b9173fad1
Abanoub Ghadban <abanoub@shakacode.com>
2024-12-11 20:58:14 +0200

    simplify rsc loader

--- spec/dummy/config/webpack/rsc-transform-loader.mjs
+++ spec/dummy/config/webpack/rsc-transform-loader.mjs
@@ -1,41 +1,16 @@
-import * as RSDWNodeLoader from 'react-server-dom-webpack/node-loader';
+import { load } from 'react-server-dom-webpack/node-loader';
+import { pathToFileURL } from 'url';
 
-export default function(source) {
+export default function (source, sourceMap) {
   const callback = this.async();
-
-  const resolve = async (specifier, { parentURL }) => {
-    if (!specifier) {
-      return {
-        url: ''
-      };
-    }
-    const url = (await this.resolve(specifier, parentURL)).id;
-    return {
-      url
-    };
-  };
-
-  const id = this.resourcePath;
-  const load = async (url) => {
-    let source1 = url === id ? source : (await this.load({
-      id: url
-    })).code;
-    // HACK move directives before import statements.
-    source1 = source1.replace(/^(import {.*?} from ".*?";)\s*"use (client|server)";/, '"use $2";$1');
+  const fileUrl = pathToFileURL(this.resourcePath).href;
+  
+  load(fileUrl, null, async () => {
     return {
       format: 'module',
-      source: source1,
-    };
-  };
-
-  RSDWNodeLoader.resolve('', {
-    conditions: ['react-server', 'workerd'],
-    parentURL: ''
-  }, resolve);
-
-  RSDWNodeLoader.load(this.resourcePath, null, load)
-    .then(({ source }) => {
-      callback(null, source);
-    })
-    .catch(callback);
-};
+      source,
+    }
+  }).then(result => {
+    return callback(null, result.source, sourceMap);
+  });
+}
