c4771b57ec174497c391f13732e8c23d668c5d9a
Abanoub Ghadban <abanoub@shakacode.com>
2025-01-12 14:01:32 +0200

    don't write client files to disk
    No needed any more because `react-client-manifest.json` is fetched from dev server when running

--- spec/dummy/config/webpack/clientWebpackConfig.mjs
+++ spec/dummy/config/webpack/clientWebpackConfig.mjs
@@ -15,14 +15,6 @@ const configureClient = () => {
 
   clientConfig.plugins.push(new RSDWPlugin({ isServer: false }));
 
-  clientConfig.devServer = {
-    ...clientConfig.devServer,
-    devMiddleware: {
-      ...clientConfig.devServer?.devMiddleware,
-      writeToDisk: true,
-    },
-  };
-
   if (!isHMR) {
     clientConfig.plugins.unshift(new LoadablePlugin({ filename: 'loadable-stats.json', writeToDisk: true }));
   }
