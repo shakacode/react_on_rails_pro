9b98410c60c318d61060343fe43970bd32972368
Abanoub Ghadban <abanoub@shakacode.com>
2025-01-14 12:13:42 +0200

    fix failing rspec tests caused by server components and rsc renderer

--- spec/dummy/client/app/ror-auto-load-components/HelloWorldHooks.jsx
+++ spec/dummy/client/app/ror-auto-load-components/HelloWorldHooks.jsx
@@ -1,21 +1,5 @@
 'use client';
 
-// Super simple example of the simplest possible React component
-import React, { useState } from 'react';
-import css from '../components/HelloWorld.module.scss';
-
-// TODO: make more like the HelloWorld.jsx
-function HelloWorldHooks(props) {
-  const [name, setName] = useState(props.helloWorldData.name);
-  return (
-    <div>
-      <h3 className={css.brightColor}>Hello, {name}!</h3>
-      <p>
-        Say hello to:
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-      </p>
-    </div>
-  );
-}
+import HelloWorldHooks from '../components/HelloWorldHooks';
 
 export default HelloWorldHooks;
