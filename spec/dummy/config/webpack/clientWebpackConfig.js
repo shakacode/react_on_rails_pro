017155f125208329ce6ad955ec3c948b2b054615
Abanoub Ghadban <abanoub@shakacode.com>
2025-01-15 15:53:15 +0200

    add a comment that shows why we removed stream module from the client bundle

--- spec/dummy/config/webpack/clientWebpackConfig.js
+++ spec/dummy/config/webpack/clientWebpackConfig.js
@@ -7,7 +7,10 @@ const isHMR = process.env.HMR;
 const configureClient = () => {
   const clientConfig = commonWebpackConfig();
 
+  // The stream module is used on the server side for streaming server side rendering.
+  // It's not needed on the client side, so we can remove it.
   clientConfig.resolve.fallback = { stream: false };
+
   // server-bundle is special and should ONLY be built by the serverConfig
   // In case this entry is not deleted, a very strange "window" not found
   // error shows referring to window["webpackJsonp"]. That is because the
