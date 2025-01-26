15d15df5e7403ed9174d345d6b04be8d63bf6679
Abanoub Ghadban <abanoub@shakacode.com>
2025-01-26 10:28:08 +0200

    Refactor stream method to use named parameter for close_stream_at_end

--- lib/react_on_rails_pro/stream.rb
+++ lib/react_on_rails_pro/stream.rb
@@ -9,9 +9,8 @@ module ReactOnRailsPro
     # Streams React components within a specified template to the client.
     #
     # @param template [String] The path to the template file to be streamed.
+    # @param close_stream_at_end [Boolean] Whether to automatically close the stream after rendering (default: true).
     # @param render_options [Hash] Additional options to pass to `render_to_string`.
-    #                             Includes :close_stream_at_end (Boolean) to control whether
-    #                             to automatically close the stream after rendering (default: true).
     #
     # components must be added to the view using the `stream_react_component` helper.
     #
@@ -19,14 +18,13 @@ module ReactOnRailsPro
     #   stream_view_containing_react_components(template: 'path/to/your/template')
     #
     # @example
-    #   stream_view_containing_react_components(template: 'path/to/your/template', layout: false)
+    #   stream_view_containing_react_components(template: 'path/to/your/template', close_stream_at_end: false, layout: false)
     #
     # @note The `stream_react_component` helper is defined in the react_on_rails gem.
     #       For more details, refer to `lib/react_on_rails/helper.rb` in the react_on_rails repository.
     #
     # @see ReactOnRails::Helper#stream_react_component
-    def stream_view_containing_react_components(template:, **render_options)
-      close_stream_at_end = render_options.delete(:close_stream_at_end) { true }
+    def stream_view_containing_react_components(template:, close_stream_at_end: true, **render_options)
       @rorp_rendering_fibers = []
       template_string = render_to_string(template: template, **render_options)
       # View may contain extra newlines, chunk already contains a newline
