fc0806f8ae0506851710380ea0d47ba8a5aa1dd3
Abanoub Ghadban <abanoub@shakacode.com>
2025-02-18 14:09:13 +0200

    Disable CSS module import in HelloWorldHooks component

--- spec/dummy/client/app/components/HelloWorldHooks.jsx
+++ spec/dummy/client/app/components/HelloWorldHooks.jsx
@@ -2,13 +2,15 @@
 
 // Super simple example of the simplest possible React component
 import React, { useState } from 'react';
-import css from './HelloWorld.module.scss';
+// TODO: CSS modules need to be configured to work properly with React Server Components (RSC)
+// and client components that are used within server components.
+// import css from './HelloWorld.module.scss';
 
 function HelloWorldHooks(props) {
   const [name, setName] = useState(props.helloWorldData.name);
   return (
     <div>
-      <h3 className={css.brightColor}>Hello, {name}!</h3>
+      <h3>Hello, {name}!</h3>
       <p>
         Say hello to:
         <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
