efad768844a1e6a5c716f85c034b431ba0bae0b4
Abanoub Ghadban <abanoub@shakacode.com>
2025-02-27 14:51:40 +0200

    Add postsCount prop to control number of posts displayed in RSC Posts Page

--- spec/dummy/client/app/ror-auto-load-components/RSCPostsPage.jsx
+++ spec/dummy/client/app/ror-auto-load-components/RSCPostsPage.jsx
@@ -5,14 +5,14 @@ import HelloWorld from '../components/HelloWorldHooks.jsx';
 import ErrorComponent from '../components/ErrorComponent.jsx';
 import Spinner from '../components/Spinner.jsx';
 
-const RSCPostsPage = ({ artificialDelay, ...props }) => {
+const RSCPostsPage = ({ artificialDelay, postsCount, ...props }) => {
   return (
     <ErrorBoundary FallbackComponent={ErrorComponent}>
       <div>
         <HelloWorld {...props} />
         <h1 style={{ fontSize: '2rem', fontWeight: 'bold' }}>RSC Posts Page</h1>
         <Suspense fallback={<Spinner />}>
-          <Posts artificialDelay={artificialDelay} />
+          <Posts artificialDelay={artificialDelay} postsCount={postsCount} />
         </Suspense>
       </div>
     </ErrorBoundary>
