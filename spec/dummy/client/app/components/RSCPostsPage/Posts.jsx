64ad3c635b13db647e9bcb1cfef3f7a5c9437095
Abanoub Ghadban <abanoub@shakacode.com>
2025-03-04 18:14:47 +0200

    linting

--- spec/dummy/client/app/components/RSCPostsPage/Posts.jsx
+++ spec/dummy/client/app/components/RSCPostsPage/Posts.jsx
@@ -8,7 +8,7 @@ const Posts = async ({ artificialDelay, postsCount = 2 }) => {
   const posts = await (await fetch(`http://localhost:3000/api/posts`)).json();
   const postsByUser = _.groupBy(posts, 'user_id');
   const onePostPerUser = _.map(postsByUser, (group) => group[0]);
-  const postsToShow = onePostPerUser.slice(0, postsCount)
+  const postsToShow = onePostPerUser.slice(0, postsCount);
 
   return (
     <div>
