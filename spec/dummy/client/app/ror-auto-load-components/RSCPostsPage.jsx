bc573d6a98d4ef175a29eb52d18c3081f7071960
Abanoub Ghadban <abanoub@shakacode.com>
2025-05-25 13:51:12 +0300

    Refactor ErrorBoundary component to use a new ErrorComponent for improved error handling and update package dependencies for better SSR support.

--- spec/dummy/client/app/ror-auto-load-components/RSCPostsPage.jsx
+++ spec/dummy/client/app/ror-auto-load-components/RSCPostsPage.jsx
@@ -2,12 +2,12 @@ import React, { Suspense } from 'react';
 import { ErrorBoundary } from '../components/ErrorBoundary';
 import Posts from '../components/RSCPostsPage/Posts';
 import HelloWorld from '../components/HelloWorldHooks.jsx';
-import ErrorComponent from '../components/ErrorComponent.jsx';
+import ErrorComponent from '../components/ErrorComponent';
 import Spinner from '../components/Spinner.jsx';
 
 const RSCPostsPage = ({ artificialDelay, postsCount, ...props }) => {
   return (
-    <ErrorBoundary FallbackComponent={ErrorComponent}>
+    <ErrorBoundary>
       <div>
         <HelloWorld {...props} />
         <h1 style={{ fontSize: '2rem', fontWeight: 'bold' }}>RSC Posts Page</h1>
