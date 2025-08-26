109cc51c7ffc088b31d0c3a623c73f48f0e3d5e3
ihabadham <ihabadham4p@gmail.com>
2025-08-26 13:28:20 +0300

    add concurrent_stream_drain flag (default false)

--- lib/react_on_rails_pro/configuration.rb
+++ lib/react_on_rails_pro/configuration.rb
@@ -53,6 +53,7 @@ module ReactOnRailsPro
     DEFAULT_RAISE_NON_SHELL_SERVER_RENDERING_ERRORS = false
     DEFAULT_ENABLE_RSC_SUPPORT = false
     DEFAULT_RSC_PAYLOAD_GENERATION_URL_PATH = "rsc_payload/"
+    DEFAULT_CONCURRENT_STREAM_DRAIN = false
 
     attr_accessor :renderer_url, :renderer_password, :tracing,
                   :server_renderer, :renderer_use_fallback_exec_js, :prerender_caching,
@@ -61,7 +62,7 @@ module ReactOnRailsPro
                   :remote_bundle_cache_adapter, :ssr_pre_hook_js, :assets_to_copy,
                   :renderer_request_retry_limit, :throw_js_errors, :ssr_timeout,
                   :profile_server_rendering_js_code, :raise_non_shell_server_rendering_errors, :enable_rsc_support,
-                  :rsc_payload_generation_url_path
+                  :rsc_payload_generation_url_path, :concurrent_stream_drain
 
     def initialize(renderer_url: nil, renderer_password: nil, server_renderer: nil, # rubocop:disable Metrics/AbcSize
                    renderer_use_fallback_exec_js: nil, prerender_caching: nil,
@@ -71,7 +72,7 @@ module ReactOnRailsPro
                    remote_bundle_cache_adapter: nil, ssr_pre_hook_js: nil, assets_to_copy: nil,
                    renderer_request_retry_limit: nil, throw_js_errors: nil, ssr_timeout: nil,
                    profile_server_rendering_js_code: nil, raise_non_shell_server_rendering_errors: nil,
-                   enable_rsc_support: nil, rsc_payload_generation_url_path: nil)
+                   enable_rsc_support: nil, rsc_payload_generation_url_path: nil, concurrent_stream_drain: DEFAULT_CONCURRENT_STREAM_DRAIN)
       self.renderer_url = renderer_url
       self.renderer_password = renderer_password
       self.server_renderer = server_renderer
@@ -94,6 +95,7 @@ module ReactOnRailsPro
       self.raise_non_shell_server_rendering_errors = raise_non_shell_server_rendering_errors
       self.enable_rsc_support = enable_rsc_support
       self.rsc_payload_generation_url_path = rsc_payload_generation_url_path
+      self.concurrent_stream_drain = concurrent_stream_drain
     end
 
     def setup_config_values
