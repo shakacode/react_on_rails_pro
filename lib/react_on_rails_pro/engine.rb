8a2c9915c829013edbb77ebd57e72aa6a41129ea
Tony Ta <tony@academia.edu>
2026-04-23 15:51:37 -0700

    Fix ScoutApm instrumentation load-order to match react_on_rails
    This commit moves the instrumentation loading into ReactOnRails::Engine
    with an initializer explicitly configured to run after ScoutApm
    initializes ("scout_apm.start"). This ensures instrumentation is
    properly installed regardless of gem ordering and only after ScoutApm is
    fully configured.
    
    This mirrors the fix in the core gem (shakacode/react_on_rails#2442).

--- lib/react_on_rails_pro/engine.rb
+++ lib/react_on_rails_pro/engine.rb
@@ -7,5 +7,16 @@ module ReactOnRailsPro
     initializer "react_on_rails_pro.routes" do
       ActionDispatch::Routing::Mapper.include ReactOnRailsPro::Routes
     end
+
+    # Install ScoutApm instrumentation after ScoutApm is configured via "scout_apm.start" initializer.
+    # https://github.com/scoutapp/scout_apm_ruby/blob/v6.1.0/lib/scout_apm.rb#L221
+    initializer "react_on_rails_pro.scout_apm_instrumentation", after: "scout_apm.start" do
+      next unless defined?(ScoutApm)
+
+      ReactOnRailsPro::ServerRenderingPool::NodeRenderingPool.singleton_class.class_eval do
+        include ScoutApm::Tracer
+        instrument_method :exec_server_render_js, type: "ReactOnRails", name: "Node React Server Rendering"
+      end
+    end
   end
 end
